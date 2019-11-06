6aa52bc01fc9de2702b8c6906f90fd138f553a09
Julia <bloom_us@mail.ru>
2019-11-06 13:09:21 +0300

    fsd

--- SNAke/SNAke/HorizontalLine.cs
+++ SNAke/SNAke/HorizontalLine.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace SNAke
 {
-    class HorizontalLine
+    class HorizontalLine : Figure
     {
-        List<Point> pList;
+        
         public HorizontalLine(int xLeft, int xRight, int y, char sym)
         {
             pList = new List<Point>();
@@ -19,13 +19,8 @@ namespace SNAke
             }
                 
         }
-        public void Drow()
-        {
-            foreach(Point p in pList)
-            {
-                p.Draw();
-            }
-        }
+        
+        
 
     }
 }
--- SNAke/SNAke/Program.cs
+++ SNAke/SNAke/Program.cs
@@ -10,13 +10,22 @@ namespace SNAke
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1,3,'*');
-            p1.Draw();
+            Console.SetWindowSize(1, 1);
+            Console.SetBufferSize(80, 25);
+            Console.SetWindowSize(80, 25);
 
-            Point p2 = new Point(4,5,'#');
-            p2.Draw();
-            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
-            line.Drow();
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
+
+
+            Point p = new Point(10, 5, '*');
+            p.Draw();
             Console.ReadLine();
         }
         
