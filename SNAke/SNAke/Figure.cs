25f812cd81f08343259711a5f7973cdcc2191382
Julia <bloom_us@mail.ru>
2019-11-06 13:09:31 +0300

    s

---
+++ SNAke/SNAke/Figure.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SNAke
+{
+    class Figure
+    {
+       protected List<Point> pList;
+
+
+
+        public void Drow()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
